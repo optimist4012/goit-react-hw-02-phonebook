c1f0e5ee1c7276439c492c71adca04a45c65ad1a
optimist4012 <131488492+optimist4012@users.noreply.github.com>
2023-12-02 20:15:26 +0200

    finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,61 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { AppLayout } from './App.styled';
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  addContact = newContact => {
+    const hasNewContactInContacts = this.state.contacts.find(
+      contact => contact.name.toLowerCase() === newContact.name.toLowerCase()
+    );
+
+    hasNewContactInContacts
+      ? alert(`${newContact.name} is already in contacts`)
+      : this.setState(prevState => {
+          return {
+            contacts: [...prevState.contacts, newContact],
+          };
+        });
+  };
+
+  deleteContact = contactId => {
+    this.setState(prevState => {
+      return {
+        contacts: prevState.contacts.filter(item => item.id !== contactId),
+      };
+    });
+  };
+
+  updateFilter = filterString => {
+    this.setState(() => {
+      return {
+        filter: filterString,
+      };
+    });
+  };
+
+  render() {
+    const { contacts, filter } = this.state;
+
+    return (
+      <AppLayout>
+        <h1>Phonebook</h1>
+        <ContactForm onAddContact={this.addContact} />
+
+        {contacts.length > 0 && (
+          <ContactList
+            contacts={contacts}
+            filter={filter}
+            onUpdateFilter={this.updateFilter}
+            onDelete={this.deleteContact}
+          ></ContactList>
+        )}
+      </AppLayout>
+    );
+  }
+}
